fdf6adac184d1fee775545586a4b6800ddd5a754
kamran <mma744237@gmail.com>
2024-02-27 00:31:52 +0600

    ready to deply

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+// let navbarMenu = document.getElementById('menu');
+// let navbarIcon = document.getElementById('navIcon');
+
+// navbarIcon.addEventListener('click', () => {
+//   navbarIcon.src = './assets/img/navbar-Icon.svg'
+//   navbarIcon.src = './assets/img/Xmark.svg'
+//   navbarMenu.classList.toggle('active')
+// })
