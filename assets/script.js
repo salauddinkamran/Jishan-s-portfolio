d8f591430a46a35123a96ec4cd165c47311e3823
kamran <mma744237@gmail.com>
2024-03-21 00:39:18 +0600

    ready to deply

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,22 @@
 
-// let navbarMenu = document.getElementById('menu');
-// let navbarIcon = document.getElementById('navIcon');
+let navbarMenu = document.getElementById('menu');
+let navbarIcon = document.getElementById('navIcon')
 
-// navbarIcon.addEventListener('click', () => {
-//   navbarIcon.src = './assets/img/navbar-Icon.svg'
-//   navbarIcon.src = './assets/img/Xmark.svg'
-//   navbarMenu.classList.toggle('active')
-// })
+navbarIcon.addEventListener('click', () => {
+  // navbarIcon.src = './assets/img/navbar-Icon.svg'
+  // navbarIcon.src = './assets/img/Xmark.svg'
+  navbarMenu.classList.toggle('active')
+  function chnging() {
+    let image = document.getElementById('navIcon').src = './assets/img/navbar-Icon.svg';
+  
+    if (image) {
+      document.getElementById('navIcon').src = './assets/img/navbar-Icon.svg'
+    } else if (!image) {
+      document.getElementById('navIcon').src = './assets/img/Xmark.svg';
+      alert(image);
+    }
+  }
+})
+chnging();
+
+
